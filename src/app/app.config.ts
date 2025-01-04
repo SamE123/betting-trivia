529129ea1aacc484d2e3de690f9d923dc9666858
Sam <scliao123@gmail.com>
2025-01-03 21:00:21 -0500

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withFetch} from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), provideAnimationsAsync()]
-};
